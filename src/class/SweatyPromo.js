89e44ec106262e8c02486595317e4913f4a5ae57
djost <djost@simplon.co>
2019-01-31 17:16:12 +0100

    first-tests

---
+++ src/class/SweatyPromo.js
@@ -0,0 +1,23 @@
+class SweatyPromoClient {
+
+    _playerList = []
+    _matrix = []
+
+
+    constructor () {
+
+    }
+
+    // authentification
+    auth () {
+
+    }
+
+    testEvent () {
+
+        var event = new Event('SSDD');
+
+        this.dispatchEvent(event);
+    }
+
+}
---
+++ src/script2.js
@@ -0,0 +1,19 @@
+/*
+
+import Game from '../sweaty-promo-lib.js'
+
+console.log(Game)
+
+var game = new Game()
+game.test()
+
+*/
+
+
+import {'sweaty-promo-client'} from '../node_modules/sweaty-promo-client/dist/sweaty-promo-client.js'
+
+console.log(Game)
+
+// var game = new SweatyPromoClient()
+
+// console.log(game);
