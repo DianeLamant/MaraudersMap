1ed8c72efa6a31259947ba45dcae5d19e11c551e
DianeLam <diane.lamant.simplon@gmail.com>
2019-02-08 17:31:56 +0100

    debut IA

--- src/assets/js/newscript.js
+++ src/assets/js/newscript.js
@@ -1,26 +1,23 @@
-// import Game from "sweaty-promo-client/src/offline";
-
 var game = new SweatyPromoClient.offline()
 
 let vide = "./src/assets/img/vide.png";
 let caca = "./src/assets/img/caca.png";
 let player = "./src/assets/img/pigeon.gif";
-// let perdu = "./src/assets/img/sweaty.png"
 let VALUE_PLAYER = 2;
 
 // var case = document.getElementById($("#7"))$(".(game.getPosition())[0]");
 
-function startButton() {
+function start() {
+    document.getElementById('btnFormat').style.visibility = 'hidden'
     game._init()
     game.on('matrix', (matrix) => {
         game._checkColision(matrix[7], [game.getPosition()]);
         if (game._checkColision(matrix[7], [game.getPosition()]) == true) {
-            // document.getElementById(7).getElementsByClassName(game.getPosition())[0].src = perdu;
             document.getElementById('gameOverMessage').style.visibility = 'visible';
             document.getElementById('gameOverMessage').style = '-webkit-animation : unzoom 0.5s';
             end();
         }
-        console.log(game._checkColision(matrix[7], [game.getPosition()]));
+        // console.log(game._checkColision(matrix[7], [game.getPosition()]));
         matrix[7][game.getPosition()] = VALUE_PLAYER;
         for (let x = 0; x < matrix.length; x++) {
             for (let y = 0; y < matrix[x].length; y++) {
@@ -36,14 +33,46 @@ function startButton() {
     })
 }
 
+function buttonJouer() {
+    document.getElementById('btnDirection').style.visibility = 'visible'
+    start()
+
+}
+
+function buttonIA() {
+    start()
+    game.on('matrix', (matrix) => {
+        for (let x = 0; x < matrix.length; x++) {
+            for (let y = 0; y < matrix[x].length; y++) {
+                if (matrix[6][game.getPosition()] == 1 && matrix[6][game.getPosition() - 1] == 0) {
+                    leftButton();
+                } else if (matrix[6][game.getPosition()] == 1 && matrix[6][game.getPosition() - 1] == 1) {
+                    rightButton();
+                } else if (matrix[6][game.getPosition()] == 1 && matrix[6][game.getPosition() + 1] == 1) {
+                    leftButton();
+                } else if (matrix[6][game.getPosition()] == 1 && matrix[6][game.getPosition() + 1] == 0) {
+                    leftButton();
+                } else if (matrix[6][game.getPosition()] == 1 && game.getPosition() == 0) {
+                    leftButton();
+                }
+            }
+        }
+        console.log(matrix[6][game.getPosition() + 1]);
+
+        console.log(game.getPosition());
+
+    })
+}
+
 function end() {
     document.getElementById('game').style.display = 'none'
     document.getElementById('gameOver').style.display = 'block'
 }
 
 function Reset() {
-    startButton()
+    document.getElementById('btnFormat').style.visibility = 'visible'
     document.getElementById('game').style.display = 'block'
+    document.getElementById('displayZone').style.visibility = 'hidden'
     document.getElementById('gameOver').style.display = 'none'
 }
 
@@ -56,5 +85,4 @@ function leftButton() {
 function rightButton() {
     game.right()
     document.querySelector("img[src='./src/assets/img/pigeon.gif']").style.transform = "scaleX(1)";
-
 }
