3d3f83f0f91b55a195e6f307e6e9e77f8a2439ed
DianeLam <diane.lamant.simplon@gmail.com>
2019-02-11 17:31:27 +0100

    IA et autre

--- src/assets/js/newscript.js
+++ src/assets/js/newscript.js
@@ -3,48 +3,64 @@ var game = new SweatyPromoClient.offline()
 let vide = "./src/assets/img/vide.png";
 let caca = "./src/assets/img/caca.png";
 let player = "./src/assets/img/pigeon.gif";
-let VALUE_PLAYER = 2;
+// let isItEnd = false;
 
-// var case = document.getElementById($("#7"))$(".(game.getPosition())[0]");
-
-function start() {
-    document.getElementById('btnFormat').style.visibility = 'hidden'
-    game._init()
-    game.on('matrix', (matrix) => {
-        game._checkColision(matrix[7], [game.getPosition()]);
-        if (game._checkColision(matrix[7], [game.getPosition()]) == true) {
-            document.getElementById('gameOverMessage').style.visibility = 'visible';
-            document.getElementById('gameOverMessage').style = '-webkit-animation : unzoom 0.5s';
-            end();
-        }
-        // console.log(game._checkColision(matrix[7], [game.getPosition()]));
-        matrix[7][game.getPosition()] = VALUE_PLAYER;
-        for (let x = 0; x < matrix.length; x++) {
-            for (let y = 0; y < matrix[x].length; y++) {
-                if (matrix[x][y] == 1) {
-                    document.getElementById(x).getElementsByClassName(y)[0].src = caca;
-                } else if (matrix[x][y] == 0) {
-                    document.getElementById(x).getElementsByClassName(y)[0].src = vide;
-                } else if (matrix[7][game.getPosition()] == 2 && game._checkColision(matrix[7], [game.getPosition()]) == false) {
-                    document.getElementById(7).getElementsByClassName(game.getPosition())[0].src = player;
-                }
+game.on('matrix', (matrix) => {
+    for (let x = 0; x < matrix.length; x++) {
+        for (let y = 0; y < matrix[x].length; y++) {
+            if (x == 7 && y == game.getPosition()) {
+                document.getElementById(x).getElementsByClassName(y)[0].src = player;
+            } else if (matrix[x][y] == 1) {
+                document.getElementById(x).getElementsByClassName(y)[0].src = caca;
+            } else if (matrix[x][y] == 0) {
+                document.getElementById(x).getElementsByClassName(y)[0].src = vide;
             }
         }
-    })
+    }
+})
+
+game.on('sweaty', () => {
+    document.getElementById('game').style.display = 'none'
+    document.getElementById('gameOver').style.display = 'block'
+    document.getElementById('gameOverMessage').style = '-webkit-animation : unzoom 0.5s';
+    // isItEnd = true;
+    game.pause()
+    console.log('perdu')
+})
+
+function start() {
+    // game.start()
+    // console.log("Hey")
+
+    // // isItEnd = false
+    // document.getElementById('btnDirection').style.visibility = 'visible'
+    // document.getElementById('btnFormat').style.visibility = 'hidden'
+    // document.getElementById('displayZone').style.visibility = 'visible'
+
 }
 
 function buttonJouer() {
+    game.start()
+    //     // timer()
     document.getElementById('btnDirection').style.visibility = 'visible'
+    document.getElementById('btnFormat').style.visibility = 'hidden'
+    document.getElementById('displayZone').style.visibility = 'visible'
     start()
-
 }
 
 function buttonIA() {
+    game.start()
+    //     // timer()
     start()
+    document.getElementById('btnDirection').style.visibility = 'hidden'
+    document.getElementById('btnFormat').style.visibility = 'hidden'
+    document.getElementById('displayZone').style.visibility = 'visible'
     game.on('matrix', (matrix) => {
         for (let x = 0; x < matrix.length; x++) {
             for (let y = 0; y < matrix[x].length; y++) {
-                if (matrix[6][game.getPosition()] == 1 && matrix[6][game.getPosition() - 1] == 0) {
+                if (matrix[6][game.getPosition()] == 1 && game.getPosition() == 0) {
+                    rightButton();
+                } else if (matrix[6][game.getPosition()] == 1 && matrix[6][game.getPosition() - 1] == 0) {
                     leftButton();
                 } else if (matrix[6][game.getPosition()] == 1 && matrix[6][game.getPosition() - 1] == 1) {
                     rightButton();
@@ -52,37 +68,48 @@ function buttonIA() {
                     leftButton();
                 } else if (matrix[6][game.getPosition()] == 1 && matrix[6][game.getPosition() + 1] == 0) {
                     leftButton();
-                } else if (matrix[6][game.getPosition()] == 1 && game.getPosition() == 0) {
-                    leftButton();
                 }
             }
         }
-        console.log(matrix[6][game.getPosition() + 1]);
-
+        console.log(matrix[6][game.getPosition()]);
         console.log(game.getPosition());
-
     })
 }
 
-function end() {
-    document.getElementById('game').style.display = 'none'
-    document.getElementById('gameOver').style.display = 'block'
-}
+// function timer() {
+//     var secs = 0;
+//     var min = 0;
+//     var timer = setInterval(function () {
+//         document.getElementById('score').innerHTML = `Temps : ${min}m ${secs++}s`;
+//         console.log(secs);
+//         if (secs == 60) {
+//             secs = 0
+//             min++;
+//         }
+//         if (isItEnd == true) {
+//             clearInterval(timer)
+//             secs = 0;
+//             min = 0;
+//         }
+//     }, 1000);
+// };
 
-function Reset() {
-    document.getElementById('btnFormat').style.visibility = 'visible'
-    document.getElementById('game').style.display = 'block'
-    document.getElementById('displayZone').style.visibility = 'hidden'
-    document.getElementById('gameOver').style.display = 'none'
-}
+
+// function Reset() {
+//     document.getElementById('btnFormat').style.visibility = 'visible'
+//     document.getElementById('game').style.display = 'block'
+//     document.getElementById('displayZone').style.visibility = 'hidden'
+//     document.getElementById('gameOver').style.display = 'none'
+//     document.getElementById('score').innerHTML = `La promo en sueur`
+// }
 
 
 function leftButton() {
     game.left()
-    document.querySelector("img[src='./src/assets/img/pigeon.gif']").style.transform = "scaleX(-1)";
+    // document.querySelector("img[src='./src/assets/img/pigeon.gif']").style.transform = "scaleX(-1)";
 }
 
 function rightButton() {
     game.right()
-    document.querySelector("img[src='./src/assets/img/pigeon.gif']").style.transform = "scaleX(1)";
+    // document.querySelector("img[src='./src/assets/img/pigeon.gif']").style.transform = "scaleX(1)";
 }
